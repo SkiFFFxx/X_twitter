d58f6f28dc576584a131c92fc5fb817b97a09cc3
SkiFFFxx <Kim_lev12@mail.ru>
2025-03-15 20:26:06 +0600

    Изменил отображение на более читаемое для использования интерфейса CRUD

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -29,18 +29,23 @@ function deletePost(id) {
             </div>
             <div class="mt-4 " v-if="posts">
                 <div class=" p-2 border-t border-gray-500" v-for="post in posts">
-                    <div class="text-sm">#{{ post.id}}</div>
+
                     <Link class="text-2xl text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)" >{{ post.title}}</Link>
                     <div>{{ post.content}}</div>
-                    <div class="text-sm text-right">{{ post.data}}</div>
-                    <div class="text-sm text-right">
-                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)" >Show</Link>
+
+                    <div class="flex gap-4 justify-end mt-2">
+                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)">Show</Link>
+                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.edit', post.id)">Edit</Link>
+                        <p
+                            @click="deletePost(post.id)"
+                            class="cursor-pointer text-red-500 hover:text-red-800"
+                        >
+                            Delete
+                        </p>
                     </div>
-                    <div class="text-sm text-right">
-                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.edit', post.id)" >Edit</Link>
-                    </div>
-                    <div class="text-sm text-right ">
-                        <p @click="deletePost(post.id)" class="cursor-pointer text-red-500 hover:text-red-800">Delete</p>
+                    <div class="text-sm flex justify-between">
+                        <span>#{{ post.id }}</span>
+                        <span>{{ post.data }}</span>
                     </div>
                 </div>
             </div >
