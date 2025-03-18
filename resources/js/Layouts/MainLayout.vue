876767b9f4f37c2e7446f6024dcf9d85e54b8d1f
SkiFFFxx <Kim_lev12@mail.ru>
2025-03-19 01:34:35 +0600

    добавил гифку кота на общий lauout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,7 +4,12 @@
 
 <template>
     <div class="w-96 mx-auto pt-8">
-         <h1 class="text-lg mb-8">My Posts Site</h1>
+        <img
+            src="https://i.gifer.com/2iFb.gif"
+            alt="Funny GIF"
+            class="w-full h-40 object-cover mb-4"
+        />
+        <h1 class="text-lg mb-8">My Posts Site</h1>
         <slot />
     </div>
 </template>
