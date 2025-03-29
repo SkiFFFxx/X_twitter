06b5543914e299f3da3a1087a6991affe51231e6
SkiFFFxx <Kim_lev12@mail.ru>
2025-03-29 12:19:29 +0500

    Add hover menu and gif changer on click

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 
+const isToggled = ref(false);
+
+const toggleGif = () => {
+    isToggled.value = !isToggled.value;
+};
 </script>
 
 <template>
     <div class="w-96 mx-auto pt-8">
         <img
-            src="https://i.gifer.com/2iFb.gif"
+            :src="isToggled
+                ? 'https://media1.tenor.com/m/lUU2wbgHrioAAAAC/konata-luckystar.gif'
+                : 'https://media1.tenor.com/m/hvU8c92x3qcAAAAC/anime.gif'"
             alt="Funny GIF"
-            class="w-full h-40 object-cover mb-4"
+            class="w-full h-40 object-cover mb-4 cursor-pointer"
+            @click="toggleGif"
         />
         <h1 class="text-lg mb-8">My Posts Site</h1>
         <slot />
@@ -15,5 +24,4 @@
 </template>
 
 <style scoped>
-
 </style>
--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
-import {Link, router} from '@inertiajs/vue3'
+import {Link, router} from '@inertiajs/vue3';
 import MainLayout from "@/Layouts/MainLayout.vue";
-
+import { ref } from 'vue';
 
 const props = defineProps({
     posts: Array
@@ -17,42 +17,47 @@ function deletePost(id) {
     }
 }
 
+const activeDropdown = ref(null);
 
-
+function toggleDropdown(id) {
+    activeDropdown.value = activeDropdown.value === id ? null : id;
+}
 </script>
 
 <template>
 
-            <h1 class="text-lg mb-4 ">Posts</h1>
-            <div>
-                <Link :href="route('post.create')" class="hover:bg-white hover:text-black block p-2 w-32 border border-sky-500 rounded-full bg-sky-500 text-center text-white">Add Post</Link>
-            </div>
-            <div class="mt-4 " v-if="posts">
-                <div class=" p-2 border-t border-gray-500" v-for="post in posts">
+    <h1 class="text-lg mb-4 ">Posts</h1>
+    <div>
+        <Link :href="route('post.create')" class="hover:bg-white hover:text-black block p-2 w-32 border border-sky-500 rounded-full bg-sky-500 text-center text-white">Add Post</Link>
+    </div>
+    <div class="mt-4 " v-if="posts">
+        <div class="p-2 border-t border-gray-500" v-for="post in posts">
+            <div class="flex justify-between items-center">
+                <Link class="text-2xl text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)">
+                    {{ post.title }}
+                </Link>
 
-                    <Link class="text-2xl text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)" >{{ post.title}}</Link>
-                    <div>{{ post.content}}</div>
-
-                    <div class="flex gap-4 justify-end mt-2">
-                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.show', post.id)">Show</Link>
-                        <Link class="text-sky-500 hover:text-sky-800" :href="route('post.edit', post.id)">Edit</Link>
-                        <p
-                            @click="deletePost(post.id)"
-                            class="cursor-pointer text-red-500 hover:text-red-800"
-                        >
-                            Delete
-                        </p>
-                    </div>
-                    <div class="text-sm flex justify-between">
-                        <span>#{{ post.id }}</span>
-                        <span>{{ post.data }}</span>
+                <div class="relative">
+                    <div @click="toggleDropdown(post.id)" class="cursor-pointer text-sky-500 hover:text-sky-800">&#x22EE;</div>
+                    <div v-if="activeDropdown === post.id"
+                         class="absolute right-0 top-6 bg-white shadow-md border rounded-md w-24 z-10">
+                        <Link class="block p-2 text-sky-500 hover:bg-sky-100" :href="route('post.show', post.id)">Show</Link>
+                        <Link class="block p-2 text-sky-500 hover:bg-sky-100" :href="route('post.edit', post.id)">Edit</Link>
+                        <p @click="deletePost(post.id)" class="block p-2 cursor-pointer text-red-500 hover:bg-red-100">Delete</p>
                     </div>
                 </div>
-            </div >
+            </div>
+
+            <div>{{ post.content }}</div>
 
 
+            <div class="text-sm flex justify-between">
+                <span>#{{ post.id }}</span>
+                <span>{{ post.data }}</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
 </style>
